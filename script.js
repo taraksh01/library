3e8cddafc575d9eb6d90b350c712cc502541e0c6
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-17 18:25:00 +0530

    Created addBookToLibrary function that add book object to myLibrary array

--- script.js
+++ script.js
@@ -7,4 +7,16 @@ function Book(title, author, noOfPages, read) {
   return { title, author, noOfPages, read };
 }
 
-console.log("I am working fine");
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+const book1 = Book("Tarak", "Also Tarak", 0, false);
+const book2 = Book("Title 1", "Author 1", 1, false);
+const book3 = Book("Title 2", "Author 2", 2, true);
+const book4 = Book("Title 3", "Author 3", 3, false);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
+console.table(myLibrary);
