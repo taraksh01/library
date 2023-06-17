67ec65cf37bfc5fb07f84db6dcc96fe74578fb33
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-17 18:17:57 +0530

    Created a array named myLibrary that is going to store all the book object and Created book funtion that returns an object with the details of book

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let myLibrary = [];
+function Book(title, author, noOfPages, read) {
+  title;
+  author;
+  noOfPages;
+  read;
+  return { title, author, noOfPages, read };
+}
+
 console.log("I am working fine");
