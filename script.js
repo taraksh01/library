a0942fdf6946d57b8e1560bd1b2576073f6a91cb
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-17 18:26:17 +0530

    Corrected the function name from Book to book

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-function Book(title, author, noOfPages, read) {
+function book(title, author, noOfPages, read) {
   title;
   author;
   noOfPages;
@@ -11,10 +11,10 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-const book1 = Book("Tarak", "Also Tarak", 0, false);
-const book2 = Book("Title 1", "Author 1", 1, false);
-const book3 = Book("Title 2", "Author 2", 2, true);
-const book4 = Book("Title 3", "Author 3", 3, false);
+const book1 = book("Tarak", "Also Tarak", 0, false);
+const book2 = book("Title 1", "Author 1", 1, false);
+const book3 = book("Title 2", "Author 2", 2, true);
+const book4 = book("Title 3", "Author 3", 3, false);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
